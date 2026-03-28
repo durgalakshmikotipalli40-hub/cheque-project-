622d64eb89aec37afa728f31440c1bd2a8a85927
Durga <durgalakshmikotipalli40@gmail.com>
2026-03-28 22:40:14 +0530

    reset repo

--- demotest.py
+++
@@ -1,9 +0,0 @@
-import imghdr
-import os
-
-folder = r"E:\Verifying bank checks using deep learning and image processing\code\chequeproject\chequeprojet\media\cheque_data\images\train"
-
-for file in os.listdir(folder):
-    if file.endswith(".jpg"):
-        f = os.path.join(folder, file)
-        print(file, "->", imghdr.what(f))
--- users/utils/test_segmentation.py
+++
@@ -1,9 +0,0 @@
-from segment import segment_cheque_regions
-
-# Pick one cheque image from your dataset
-img_path = r"E:\Verifying bank checks using deep learning and image processing\code\check_classification\media\cheque_data\images\train\Cheque083654.jpg"
-
-# Output folder
-output_path = r"E:\Verifying bank checks using deep learning and image processing\code\check_classification\media\segmented_regions"
-
-segment_cheque_regions(img_path, output_path)
